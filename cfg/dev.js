213d4aa3f6d5cb86612dfe88183f01e32b281c89
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-11-13 17:10:56 +0200

    moved loaders to base config

--- cfg/dev.js
+++ cfg/dev.js
@@ -31,27 +31,4 @@ config.module.loaders.push({
   include: path.join(__dirname, '/../src')
 });
 
-config.module.loaders.push([
-    {
-      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff"
-    },
-    {
-      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff"
-    },
-    {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/octet-stream"
-    },
-    {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "file"
-    },
-    {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=image/svg+xml"
-    }
-]);
-
 module.exports = config;
